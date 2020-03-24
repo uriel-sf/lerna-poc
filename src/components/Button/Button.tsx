6a795d8f4f73f399b55e53034b8bfca0829fb31c
Przemek Ciąćka <przemyslaw.ciacka@superformula.com>
2020-03-24 16:41:16 +0100

    feat: Use Text component within Button.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
 import { styled } from "../Theme";
+import Text from "../Text";
 
-const Button = styled.button`
+const StyledButton = styled.button`
     background-color: ${p => p.theme.colors.primary};
     color: ${p => p.theme.colors.foreground}
 `;
+
+const Button = (props: { children: any }) => {
+    const { children } = props;
+    return (
+        <StyledButton>
+            <Text>{children}</Text>
+        </StyledButton>
+    );
+};
 
 export default Button;
