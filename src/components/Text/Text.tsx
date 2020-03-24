426cd65bcc2a9a0a561112233b77a8e1c3bd3dea
Przemek Ciąćka <przemyslaw.ciacka@superformula.com>
2020-03-24 17:43:27 +0100

    Add new components.

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,5 +1,9 @@
-import { styled } from "../Theme";
+import React from 'react';
+// import { styled } from "../Theme";
 
-const Text = styled.span``;
+const Text = (props : { children: any; }) => {
+    const { children } = props;
+    return <span>{children}</span>;
+};
 
 export default Text;
