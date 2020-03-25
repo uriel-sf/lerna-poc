e943fb968a226fcc201fa6d960e55bf891d3245e
Mateusz Blyszcz <mat@superformula.com>
2020-03-25 17:45:43 +0100

    Update text font size

--- packages/Text/src/Text.tsx
+++ packages/Text/src/Text.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { TextProps } from "./Text.types";
 
 export const Text = ({ children }: TextProps) => {
-  return <p>{children}</p>;
+  return <p style={{ fontSize: 16 }}>{children}</p>;
 };
