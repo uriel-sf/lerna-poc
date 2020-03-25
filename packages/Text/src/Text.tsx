b5ba1d641d5666d274b23d6acf2c4a66c02097a7
Mateusz Blyszcz <mat@superformula.com>
2020-03-25 18:17:14 +0100

    Bigger font size in Text component

--- packages/Text/src/Text.tsx
+++ packages/Text/src/Text.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { TextProps } from "./Text.types";
 
 export const Text = ({ children }: TextProps) => {
-  return <p style={{ fontSize: 19 }}>{children}</p>;
+  return <p style={{ fontSize: 20 }}>{children}</p>;
 };
