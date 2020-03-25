959f2dd6cf7c77027c6088d7ec77d6a0c6d189c3
Mateusz Blyszcz <mat@superformula.com>
2020-03-25 18:16:25 +0100

    Add my-awesome-lib-text-button to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Button } from "my-awesome-lib-button";
+import { TextButton } from "my-awesome-lib-text-button";
 
 function App() {
-  return <Button>Test</Button>;
+  return (
+    <>
+      <Button>Test</Button>
+      <TextButton />
+    </>
+  );
 }
 
 export default App;
